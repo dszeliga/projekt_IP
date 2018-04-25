226791140fc4c006685128777ed420a9855d7fba
sabinakam <sabikam618@student.polsl.pl>
2018-04-25 22:41:48 +0200

    obrazki level 3

--- app/src/main/java/com/example/lastwerewolf/projekt_ip/Database_three_level.java
+++ app/src/main/java/com/example/lastwerewolf/projekt_ip/Database_three_level.java
@@ -16,6 +16,18 @@ public class Database_three_level {
             R.drawable.r11,
             R.drawable.r12,
             R.drawable.r13,
+            R.drawable.r14,
+            R.drawable.r15,
+            R.drawable.r16,
+            R.drawable.r17,
+            R.drawable.r18,
+            R.drawable.r19,
+            R.drawable.r20,
+            R.drawable.r21,
+            R.drawable.r22,
+            R.drawable.r23,
+            R.drawable.r24,
+            R.drawable.r25,
 
     };
 
@@ -32,7 +44,18 @@ public class Database_three_level {
            "6",
            "3",
            "7",
-
+           "1",
+           "1",
+           "4",
+           "4",
+           "5",
+           "5",
+           "5",
+           "5",
+           "1",
+           "6",
+           "4",
+           "3",
 
     };
 
