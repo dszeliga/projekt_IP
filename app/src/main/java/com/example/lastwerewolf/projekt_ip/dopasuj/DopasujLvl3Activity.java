7ac2f5293602f6fb14a418d507dcb96fe5805541
AniaMat <a.b.matyja@gmail.com>
2018-05-17 00:02:37 +0200

    Painting matches

--- app/src/main/java/com/example/lastwerewolf/projekt_ip/dopasuj/DopasujLvl3Activity.java
+++ app/src/main/java/com/example/lastwerewolf/projekt_ip/dopasuj/DopasujLvl3Activity.java
@@ -1,11 +1,19 @@
 package com.example.lastwerewolf.projekt_ip.dopasuj;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.lastwerewolf.projekt_ip.R;
 
@@ -22,7 +30,9 @@ public class DopasujLvl3Activity extends AppCompatActivity implements View.OnCli
 
     ImageView rightV[];
 
-    int matching[] = {0, 0, 0};
+    ImageView mainView;
+
+    int matching[] = {-1, -1, -1};
     ImageView activeView = null; // maybe view reference?
 
     List<ProfessionType> leftP;
@@ -58,6 +68,7 @@ public class DopasujLvl3Activity extends AppCompatActivity implements View.OnCli
     }
 
     void setViews() {
+        mainView = findViewById(R.id.dopasuj_lvl3_wrapper);
         rightV = new ImageView[] {
             findViewById(R.id.imageView19),
             findViewById(R.id.imageView21),
@@ -94,30 +105,145 @@ public class DopasujLvl3Activity extends AppCompatActivity implements View.OnCli
         setViews();
     }
 
-    @Override
-    public void onClick(View v) {
-        boolean isLeft = false;
-        int index = -1;
+    private int getViewMiddleX(View v) {
+        Rect rect = new Rect();
+        v.getGlobalVisibleRect(rect);
+        return rect.left + rect.width()/2;
+    }
+
+    private int getViewMiddleY(View v) {
+        Rect rect = new Rect();
+        v.getGlobalVisibleRect(rect);
+        return rect.top + rect.height()/2;
+    }
+
+    boolean isLeft(View v) {
         for(int i = 0; i < leftV.length; i++) {
             if(leftV[i].getId() == v.getId()) {
-                isLeft = true;
-                index = i;
-                break;
+                return true;
             }
         }
-        if(isLeft == false) {
-            for (int i = 0; i < rightV.length; i++) {
-                if(rightV[i].getId() == v.getId()) {
-                    index = i;
-                    break;
-                }
+        return false;
+    }
+
+    int getPoz(View v) {
+        boolean isLeft = isLeft(v);
+        ImageView iva[] = isLeft ? leftV : rightV;
+        for(int i = 0; i < leftV.length; i++) {
+            if(iva[i].getId() == v.getId()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void finishGame() {
+        Toast.makeText(this, "Gratulacje!", Toast.LENGTH_SHORT).show();
+        for(int i = 0; i < matching.length; i++) {
+            matching[i] = -1;
+        }
+
+        //TODO: dodaj punkty!
+
+        // Wyczyść bitmapę i przygotuj nową grę ręcznie.
+//        setViews();
+//        Rect mrect = new Rect();
+//        mainView.getLocalVisibleRect(mrect);
+//        Bitmap bmp = Bitmap.createBitmap(mrect.width(), mrect.height(), Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(bmp);
+//        Paint p = new Paint();
+//        p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+//        p.setColor(Color.GREEN);
+//        canvas.drawLine(0,10,100,100, p);
+//        canvas.translate(mrect.left, mrect.top);
+//        mainView.draw(canvas);
+//        mainView.setImageBitmap(bmp);
+//        mainView.invalidate();
+//        canvas.drawColor(Color.BLACK, PorterDuff.Mode.CLEAR);
+
+        // Albo odpal recreate, aby rozpocząć grę od nowa.
+         this.recreate();
+    }
+
+    @Override
+    public void onClick(View v) {
+        boolean vIsLeft = isLeft(v);
+        int vIndex = getPoz(v);
+
+        if(activeView == null) {
+            activeView = (ImageView) v;
+            activeView.setBackground(getDrawable(R.drawable.ic_green_circle));
+            return;
+        }
+
+        if(activeView.getId() == v.getId()) {
+            return;
+        }
+
+        boolean aIsLeft = isLeft(activeView);
+        int aIndex = getPoz(activeView);
+
+        activeView.setBackground(null);
+        if(vIsLeft == aIsLeft) {
+            activeView = (ImageView) v;
+            activeView.setBackground(getDrawable(R.drawable.ic_green_circle));
+            return;
+        }
+        activeView = null;
+
+        // Make connections
+        int mIndex = aIsLeft ? aIndex : vIndex;
+        int mVal = aIsLeft ? vIndex : aIndex;
+        matching[mIndex] = mVal;
+        for(int i = 0; i < matching.length; i++) {
+            if(i != mIndex && matching[i] == mVal) {
+                matching[i] = -1;
             }
         }
 
-        if(activeView != null) {
-            activeView.setBackground(null);
+        // Print connections
+        Rect mrect = new Rect();
+        mainView.getLocalVisibleRect(mrect);
+        Bitmap bmp = Bitmap.createBitmap(mrect.width(), mrect.height(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        Paint p = new Paint();
+        p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        canvas.translate(mrect.left, mrect.top);
+        mainView.draw(canvas);
+        mainView.setImageBitmap(bmp);
+        p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.ADD));
+        p.setColor(Color.BLACK);
+        p.setStrokeWidth(20);
+        canvas.drawColor(Color.BLACK, PorterDuff.Mode.CLEAR);
+        int correct = 0;
+        for(int i = 0; i < matching.length; i++) {
+            if(matching[i] >= 0) {
+                if(leftP.get(i) == rightP.get(matching[i])) {
+                    correct += 1;
+                    p.setColor(Color.GREEN);
+                } else {
+                    p.setColor(Color.GRAY);
+                }
+
+                View a = leftV[i];
+                View b = rightV[matching[i]];
+                int xa = getViewMiddleX(a);
+                int ya = getViewMiddleY(a);
+
+                int xb = getViewMiddleX(b);
+                int yb = getViewMiddleY(b);
+
+                canvas.drawLine(xa, ya, xb, yb, p);
+            }
         }
-        v.setBackground(getDrawable(R.drawable.ic_green_circle));
-        activeView = (ImageView) v;
+
+        canvas.translate(mrect.left, mrect.top);
+        mainView.draw(canvas);
+        mainView.setImageBitmap(bmp);
+        mainView.invalidate();
+
+        if(correct == matching.length) {
+            finishGame();
+        }
     }
 }
